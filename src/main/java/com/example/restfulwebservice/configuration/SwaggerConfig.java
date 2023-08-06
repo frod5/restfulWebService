444f27a7aaadd6f85b63bb54d116feeb43279832
sunwooKim <frod5@kakao.com>
2023-08-06 23:54:15 +0900

    REST API Documentation을 위한 Swagger 사용

---
+++ src/main/java/com/example/restfulwebservice/configuration/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.example.restfulwebservice.configuration;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(
+            List.of("application/json"));
+
+    @Bean
+    public Docket api() {
+        /*return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);*/
+        return new Docket(DocumentationType.SWAGGER_2);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Restful-Web-Service")
+                .description("Restful-Web-Service API")
+                .version("1.0.0")
+                .build();
+    }
+}
