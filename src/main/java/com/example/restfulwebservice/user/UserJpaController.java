62ff6c2946e405e97337986d61803b3f271fb6f3
sunwooKim <frod5@kakao.com>
2023-08-07 23:57:41 +0900

    JPA를 이용한 사용자 추가와 삭제 - POST/DELETE HTTP Method

--- src/main/java/com/example/restfulwebservice/user/UserJpaController.java
+++ src/main/java/com/example/restfulwebservice/user/UserJpaController.java
@@ -3,11 +3,12 @@ package com.example.restfulwebservice.user;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -32,5 +33,22 @@ public class UserJpaController {
 
         //HATEOAS
         return EntityModel.of(user, linkTo(methodOn(UserJpaController.class).retrieveAllUsers()).withRel("all-users"));
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userJpaRepository.deleteById(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User saveEntity = userJpaRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("{id}")
+                .buildAndExpand(saveEntity.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into users values (1, now(), 'USER1', 'test1111', '701010-1111111');
-insert into users values (2, now(), 'USER2', 'test2222', '801010-2222222');
-insert into users values (3, now(), 'USER3', 'test3333', '901010-3333333');
+insert into users values (90001, now(), 'USER1', 'test1111', '701010-1111111');
+insert into users values (90002, now(), 'USER2', 'test2222', '801010-2222222');
+insert into users values (90003, now(), 'USER3', 'test3333', '901010-3333333');
