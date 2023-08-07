34fe81f1f1e90b4545293bb5a21fcfe9518ce4fd
sunwooKim <frod5@kakao.com>
2023-08-07 23:46:19 +0900

    JPA를 이용한 사용자 목록 조회 - GET HTTP Method 및 HATEOAS

--- src/main/java/com/example/restfulwebservice/user/UserJpaController.java
+++ src/main/java/com/example/restfulwebservice/user/UserJpaController.java
@@ -2,11 +2,16 @@ package com.example.restfulwebservice.user;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +24,13 @@ public class UserJpaController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return userJpaRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable Long id) {
+        User user = userJpaRepository.findById(id).orElseThrow(() -> new UserNotFoundException(String.format("ID[%s] not found", id)));
+
+        //HATEOAS
+        return EntityModel.of(user, linkTo(methodOn(UserJpaController.class).retrieveAllUsers()).withRel("all-users"));
     }
 }
